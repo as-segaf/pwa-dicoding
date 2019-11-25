3c54d7cfdddc62aaab6d53eb019003211d02defd
as-segaf <ziyanassegaf20@gmail.com>
2019-11-25 10:29:34 +0700

    new reader app

--- news-reader/service-worker.js
+++ news-reader/service-worker.js
@@ -1,14 +1,16 @@
-const CACHE_NAME = "firstpwa";
+const CACHE_NAME = "newsreader";
 var urlsToCache = [
     "/",
     "./nav.html",
     "./index.html",
+    "./article.html",
     "./pages/home.html",
     "./pages/about.html",
     "./pages/contact.html",
     "./css/materialize.min.css",
     "./js/materialize.min.js",
     "./js/nav.js",
+    "./js/api.js",
     "icon.png",
     "./manifest.json"
 ];
@@ -36,21 +38,43 @@ self.addEventListener("activate", function(event){
     );
 });
 
-self.addEventListener("fetch", function(event){
-    event.respondWith(
-        caches
-            .match(event.request, { cacheName: CACHE_NAME })
-            .then(function (response) {
-                if (response) {
-                    console.log("Service Worker: Gunakan asset dari cache: ", response.url);
-                    return response;
-                }
+// self.addEventListener("fetch", function(event){
+//     event.respondWith(
+//         caches
+//             .match(event.request, { cacheName: CACHE_NAME })
+//             .then(function (response) {
+//                 if (response) {
+//                     console.log("Service Worker: Gunakan asset dari cache: ", response.url);
+//                     return response;
+//                 }
 
-                console.log(
-                    "Service Worker: Memuat asset dari server: ",
-                    event.request.url
-                );
-                return fetch(event.request);
+//                 console.log(
+//                     "Service Worker: Memuat asset dari server: ",
+//                     event.request.url
+//                 );
+//                 return fetch(event.request);
+//             })
+//     );
+// });
+
+
+self.addEventListener("fetch", function(event) {
+    var base_url = "https://readerapi.codepolitan.com/";
+
+    if (event.request.url.indexOf(base_url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_NAME).then(function(cache) {
+                return fetch(event.request).then(function(response) {
+                    cache.put(event.request.url, response.clone());
+                    return response;
+                })
             })
-    );
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request, { ignoreSearch: true}).then(function(response){
+                return response || fetch (event.request);
+            })
+        )
+    }
 });
